9a24f5ce11d987f3c4819b669bba5fc45cf94ea3
sbrezdeniuk87 <sbrezdeniuk@gmail.com>
2018-10-25 11:41:46 +0300

    add form registration

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,36 @@
+﻿import React from 'react'
+import classes from './Input.css'
+
+function isInvalid({valid, touched, shouldValidate}){
+	return !valid && shouldValidate && touched
+}
+
+const Input = props =>{
+
+	const inputType = props.type || 'text'
+	const cls = [classes.Input]
+	const htmlForm = `${inputType}-${Math.random()}`
+
+	if(isInvalid(props)){
+		cls.push(classes.invalid)
+	}
+
+	return(
+		<div className={cls.join(' ')}>
+			<label htmlform={htmlForm}>{props.label}</label>
+			<input 
+				type={inputType}
+				id={htmlForm}
+				value={props.value}
+				onChange={props.onChange}
+			/>
+			{
+				isInvalid(props)
+				? <span>{props.errorMessage || 'Введите верное значение'}</span>
+				: null
+			}
+		</div>
+	)
+}
+
+export default Input
