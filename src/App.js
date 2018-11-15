3137caf060d8932cd5507d68335dab9a34b9148c
sbrezdeniuk87 <sbrezdeniuk@gmail.com>
2018-11-15 12:16:20 +0200

    change routers for login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import Auth from './containers/Auth/Auth'
 import Registration from './containers/Registration/Registration'
 import Profile from './containers/Profile/Profile'
 import PlayTable from './containers/PlayTable/PlayTable'
 
 class App extends Component {
+  state = {
+    userId: ''
+  } 
+
+  componentDidMount(){
+    const userId = localStorage.getItem('userId');
+    this.setState({
+      userId
+    })
+  }
   render() {
-    return (
-      <Switch>
-        <Route path="/" exact component={Auth} />
-        <Route path="/registration" component={Registration} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/play" component={PlayTable} />
-      </Switch>
-    );
+   
+    if(this.state.userId){
+      return (
+        <Switch>          
+          <Route path="/registration" component={Registration} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/play" component={PlayTable} />
+          <Route path="/" exact component={Auth} />
+        </Switch>
+      ); 
+    }else{
+      return (
+        <Switch>
+          <Route path="/registration" component={Registration} /> 
+          <Route path="/" exact component={Auth} />
+          
+          <Redirect to="/" />         
+        </Switch>
+      ); 
+    }
+    
   }
 }
 
